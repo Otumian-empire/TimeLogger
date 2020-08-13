49c3583c2967d84759f0a5912be5c2401a0de862
otumian <popecan1000@gmail.com>
2020-08-13 21:05:32 +0000

    TimeLogger

--- timeapp/urls.py
+++ timeapp/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # reads all users and tasks
     path('', views.index, name='index'),
     path('startwork/', views.start_work, name='start_work'),
+    path('taskstats/', views.stats, name='task_stats'),
     path('userstats/', views.user_stats, name='user_stats'),
+    path('user/<id>/', views.user_profile, name='user_profile'),
 ]
--- timeapp/views.py
+++ timeapp/views.py
@@ -9,8 +9,7 @@ User = get_user_model()
 
 def index(request):
     users = User.objects.all().filter
-    tasks = Task.objects.all()
-    user_tasks = UserTaskJunction.objects.all()
+    user_tasks = UserTaskJunction.objects.all()[:5]
 
     context = {
         'title': 'Users and Task',
@@ -26,6 +25,81 @@ def start_work(request):
     return render(request, 'timeapp/start_work.html', context=context)
 
 
+class TaskDev:
+    def __init__(self, task, user_list=[]):
+        self.task = task
+        self.user_list = user_list
+
+    @property
+    def get_task(self):
+        return self.task
+
+    @property
+    def get_dev_list(self):
+        return self.user_list
+
+
+def stats(request):
+    tasks = Task.objects.all()
+    users = User.objects.all()
+    user_tasks = UserTaskJunction.objects.all()
+
+    data = []
+
+    for task in tasks:
+        data.append(TaskDev(task))
+
+    for item in data:
+        for user_task in user_tasks:
+            if item.get_task.id == user_task.tid:
+                item.get_dev_list.append(user_task.uid)
+
+    for item in data:
+        task, dev = item.get_task, item.user_list
+        print(task, dev)
+
+    context = {
+        'title': "user stats",
+        "data": []
+    }
+
+    return render(request, 'timeapp/stats.html', context=context)
+
+
 def user_stats(request):
     context = {'title': "user stats"}
     return render(request, 'timeapp/stats.html', context=context)
+
+
+def user_profile(request, id):
+    user = User.objects.get(id=id)
+
+    context = {
+        'title': "Profile page",
+        'user': user
+    }
+
+    if request.method == "POST":
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        # user.username = request.POST['username']
+
+        user.save()
+
+    return render(request, 'timeapp/user_profile.html', context=context)
+
+
+# [
+#     {
+#         "task": id,
+#         "devs": []
+#     },
+#     {
+#         "task": id,
+#         "devs": []
+#     },
+# ]
+
+
+# TaskDev(1, [1, 2, 3])
